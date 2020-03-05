da868b923a77ac332f798694c603773a0feb0b53
iijaachok <8614062+iijaachok@users.noreply.github.com>
2020-03-05 02:07:37 +0100

    Initial commit
    Signed-off-by: iijaachok <8614062+iijaachok@users.noreply.github.com>

---
+++ server.js
@@ -0,0 +1,10 @@
+const {ApolloServer} = require('apollo-server');
+const {schema} = require('./graphql');
+
+const server = new ApolloServer({
+  schema
+});
+
+server.listen().then(({url}) => {
+  console.log(`🚀 Server ready at ${url}`);
+});
